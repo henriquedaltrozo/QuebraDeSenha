45b87d2b72a9b82829d64e74692e352dee9a2d91
Victor Ribeiro <167916352+vfrale@users.noreply.github.com>
2025-08-25 11:23:01 -0300

    Update seq.c

--- src/seq.c
+++ src/seq.c
@@ -17,7 +17,7 @@ void gerar_senha(char *tentativa, int pos) {
     if (pos == MAX_LEN) {
         tentativa[pos] = '\0';
         if (strcmp(tentativa, PASSWORD) == 0) {
-            printf("Senha encontrada: %s\n", tentativa);
+            //printf("Senha encontrada: %s\n", tentativa);
             achou = 1;
         }
         return;
